cbaca27abffa70ad74edf7974dfaf360a5c52a75
Toby the potato <tobyclh@gmail.com>
2017-07-08 14:35:15 +0900

    undefine atomic for new GPUs

--- include/caffe/common.cuh
+++ include/caffe/common.cuh
@@ -4,18 +4,22 @@
 #define CAFFE_COMMON_CUH_
 
 #include <cuda.h>
-
-// CUDA: atomicAdd is not defined for doubles
-static __inline__ __device__ double atomicAdd(double *address, double val) {
-  unsigned long long int* address_as_ull = (unsigned long long int*)address;
+#if !defined(__CUDA_ARCH__) || __CUDA_ARCH__ >= 600
+#else
+CUDA: atomicAdd is not defined for doubles
+static __inline__ __device__ double atomicAdd(double *address, double val)
+{
+  unsigned long long int *address_as_ull = (unsigned long long int *)address;
   unsigned long long int old = *address_as_ull, assumed;
-  if (val==0.0)
+  if (val == 0.0)
     return __longlong_as_double(old);
-  do {
+  do
+  {
     assumed = old;
-    old = atomicCAS(address_as_ull, assumed, __double_as_longlong(val +__longlong_as_double(assumed)));
+    old = atomicCAS(address_as_ull, assumed, __double_as_longlong(val + __longlong_as_double(assumed)));
   } while (assumed != old);
   return __longlong_as_double(old);
 }
+#endif
 
 #endif
